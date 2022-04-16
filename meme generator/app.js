d7ff221218ce2f40015394ca16f38e2a88a7d4bf
d3nnysm2904 <dennymosquera@outlook.com>
2022-04-15 21:49:37 -0400

    meme

--- meme generator/app.js
+++ meme generator/app.js
@@ -1,4 +1,12 @@
-const imgInput = document.querySelector("#inputImage");
+const imgUrl = document.querySelector("#text");
+const buttonCreate = document.querySelector("#imgFile");
+const memeImg = document.querySelector("#meme-img");
 
-console.log(imgInput.value);
-setInterval(() => {}, interval);
+console.log(imgUrl.value);
+buttonCreate.addEventListener("click", function createImg(e) {
+  e.preventDefault();
+  const newImg = document.createElement("img");
+  newImg.setAttribute("src", imgUrl.value);
+  newImg.classList.add("newImg");
+  memeImg.appendChild(newImg);
+});
