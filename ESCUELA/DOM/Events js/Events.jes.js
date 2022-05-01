b03f346a5721bcbc1936e89c90a5660d4d0d308d
d3nnysm2904 <dennymosquera@outlook.com>
2022-05-01 19:11:16 -0400

    jquery

--- ESCUELA/DOM/Events js/Events.jes.js
+++ ESCUELA/DOM/Events js/Events.jes.js
@@ -1,60 +1,55 @@
-
-//we create a function that makes body purple 
-function purpleBody (){
-    document.body.style.backgroundColor='purple '
-
+//we create a function that makes body purple
+function purpleBody() {
+  document.body.style.backgroundColor = "purple ";
 }
 
-//abnother function that wen we type a color makes the body that color 
-function colors(color){
-    document.body.style.backgroundColor=color;
-
+//abnother function that wen we type a color makes the body that color
+function colors(color) {
+  document.body.style.backgroundColor = color;
 }
-//select the button with id of red an passe the function colors on event click 
-const btn = document.querySelector('#red')
+//select the button with id of red an passe the function colors on event click
+const btn = document.querySelector("#red");
 
-btn.addEventListener('click',function(){
+btn.addEventListener("click", function () {
+  colors("red");
+});
 
-colors('red')}) 
+//adding a click and double click to button yellow and violet
+const violetClick = document.querySelector("#violet");
 
+violetClick.addEventListener("dblclick", function () {
+  //document.body.style.backgroundColor='violet'
+  colors("violet");
+});
 
-//adding a click and double click to button yellow and violet 
-const violetClick= document.querySelector('#violet')
+const h1 = document.querySelector("h1");
+violetClick.addEventListener("click", function () {
+  h1.style.color = "cyan";
+  colors("yellow");
+});
 
-violetClick.addEventListener('dblclick', function (){
-//document.body.style.backgroundColor='violet'
-colors('violet')
-})
+//load events
+//this is to leave the script tag up top and run the java inside the DOmcontentloaded
+document.addEventListener("DOMContentLoaded", function () {
+  const bnt = document.querySelector("#load");
 
-const h1=document.querySelector('h1')
-violetClick.addEventListener('click' ,function (){
-    h1.style.color='cyan';
-    colors('yellow')
-})
+  bnt.addEventListener("click", function () {
+    console.log("Clicked");
+  });
+});
 
-//load events 
-//this is to leave the script tag up top and run the java inside the DOmcontentloaded 
- document.addEventListener('DOMContentLoaded', function(){
- const bnt = document.querySelector('#load');
+//this will  wait for the web to complletly load
+windows.addEventListener("load", function () {
+  console.log("loaded");
+});
 
- bnt.addEventListener('click', function(){
-    console.log('Clicked');
- })
- })
+//event object
 
-//this will  wait for the web to complletly load 
- windows.addEventListener('load',function(){
-     console.log('loaded')
- })
+const p = document.querySelector("#par");
+p.addEventListener("click", function (e) {
+  console.log(e, pageX);
+});
 
- //event object
-
- const p = document.querySelector('#par')
-p.addEventListener('click',function(e){
-    console.log(e,pageX)
-
-})
-
-p.addEventListener('mousedown',function(e){
-    console.log(e,type)
-})
+p.addEventListener("mousedown", function (e) {
+  console.log(e, type);
+});
--- ESCUELA/jquery/app.js
+++ ESCUELA/jquery/app.js
@@ -28,4 +28,104 @@ $("img").attr(objImg); //this selects all img and changes all hrfef
 $("inputs").eq(0); //gets index of whatever we are selecting
 
 $("inputs").val(); //gets the value from inputs
-$("inputs").val();
+$("inputs").val("something "); //changes the value of input
+
+//css
+$("a").css("color"); //retrieves the color
+$("a").css("color", "yellow"); //changes color to yellow
+
+// classes  method
+$("h1").addClass("highligth"); //add a class to h1 there is only one but if more add to all
+
+$("h1").removeClass("highlight"); //removes class
+$("h1").toggleClass("highligth"); //on and of
+
+//Method chaining
+//jQuery return obj so allow to chain
+
+const $h1 = $("h1");
+
+$h1
+  .addClass("highligth")
+  .css("background-color", "teal")
+  .css("font-size", "40px")
+  .text("Chaining is fun");
+
+//Jquery methods traversal
+
+const specialLi = $("li").eq(3); //selects an index
+specialLi.next(); //selects the next li
+specialLi.prev(); //selects the previos
+
+specialLi.parent(); //get the parent the ul
+
+specialLi.children(); //gets the children elements
+specialLi.children("a"); //loooks for anchor tags inside li
+
+$("ul").find("a"); //it find all anchor tags
+
+//jQuery creating_appending_removing elemnts
+
+$("ul").append("<li>I AM NEW </li> ");
+
+$("li").prepend('<input type ="checkbox"/>');
+
+$("<h1>Hello</h1>").css("color", "orange").appendTo("p");
+
+$("li").after("<bold>Hi</bold>");
+
+//jQuery events
+
+//this is like addEventListener
+// $("input").click(function () {
+//   alert("Clicked input");
+// });
+
+//same as up
+$("h1").on("click", function () {
+  console.log("another click");
+});
+
+//delegation
+$("img").on("mouseleave", function () {
+  console.log(this.src);
+});
+
+$("img").on("mouseenter", function () {
+  // this.css("border", "1px solid blue") doesent work;
+  $(this).css("border", "10px solid red");
+});
+
+//button at the end
+$("#add-input").on("click", function () {
+  $("form").append('<input type="text"/>');
+});
+
+//event delegation , even thoug the new input are created, this eventlistener is listening to all inputs after the form
+$("form").on("focus", "input", function () {
+  $(this).val("Changed value");
+});
+
+//jQuery animations effects
+
+//function $(this).remove inside fadeOut is because if remove is chained to faddeOut , there is no transition
+// $("img").on("click", function () {
+//   $(this).fadeOut(3000, function () {
+//     $(this).remove();
+//   });
+// });
+//fadeIn
+
+$("img").on("click", function () {
+  $(this).animate(
+    {
+      opacity: 0,
+      width: "50px", //do not animate width (poor perfomance)
+      // transition: "5s",
+    },
+    3000,
+    function () {
+      $(this).remove(s);
+    }
+  );
+});
