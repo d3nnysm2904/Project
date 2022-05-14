7db17c30157ee2163955390b39af38f32ede3d87
d3nnysm2904 <dennymosquera@outlook.com>
2022-05-14 11:31:17 -0400

    axios

--- ESCUELA/how the web works/exercise/app.js
+++ ESCUELA/how the web works/exercise/app.js
@@ -1,27 +1,32 @@
 console.log("Let's get this party started!");
 
-const key = "WlA6pT650MmIpl36cMr6HMNkraYIWuRz";
 
 async function getApi() {
-  const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
-    params: {
-      api_key: key,
-      q: "burrito",
-      limit: 1,
-    },
-  });
-  const imgSrc = res.data.data[0].url;
-  console.log(imgSrc);
+  const key = "WlA6pT650MmIpl36cMr6HMNkraYIWuRz";
+  const input = document.querySelector("#input");
+  const search = input.value;
+
+  const res = await axios.get(
+    "https://api.giphy.com/v1/gifs/search?api_key=WlA6pT650MmIpl36cMr6HMNkraYIWuRz&q=meme&limit=25&offset=0&rating=g&lang=en" // {
+    //params: {
+    // api_key: key,
+    // q: search,
+    // limit: 1,
+    //},}
+  );
+  //const imgSrc = res.data.data[0].url;
+  // console.log(imgSrc);
   console.log(res);
-
-  const btn = document.querySelector("#btn");
-
-  btn.addEventListener("click", function () {
-    const newImg = document.createElement("img");
-    newImg.src = imgSrc;
-    const div = document.querySelector("#container");
-    div.append(newImg);
-  });
 }
 
-getApi();
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function (e) {
+  e.preventDefalut();
+  const newImg = document.createElement("img");
+  newImg.src = imgSrc;
+  const div = document.querySelector("#container");
+  div.append(newImg);
+
+  getApi();
+});
